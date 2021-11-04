8ee28ca0a94530df94ab1c4e66b32e44891167c7
Serra <pacoserranox@gmail.com>
2021-11-04 19:25:31 +0100

    commit

--- FundamentosLenguaje/Program.cs
+++ FundamentosLenguaje/Program.cs
@@ -8,8 +8,88 @@ namespace FundamentosLenguaje
         {
             //DESDE AQUI EJECUTAMOS EL METODO
             //NumeroPositivoNegativo();
-            //ESTO es un cambio
-            MayorTresNumeros();
+            //MayorTresNumeros();
+            //DiaNacimientoSemana();
+            ConjeturaCollatz();
+        }
+
+        static void ConjeturaCollatz()
+        {
+            Console.WriteLine("Introduzca un número");
+            String dato = Console.ReadLine();
+            int numero = int.Parse(dato);
+            while (numero != 1)
+            {
+                if (numero % 2 == 0)
+                {
+                    //PAR
+                    numero = numero / 2;
+                }
+                else
+                {
+                    numero = numero * 3 + 1;
+                }
+                Console.WriteLine(numero);
+            }
+        }
+
+        static void DiaNacimientoSemana()
+        {
+            Console.WriteLine("Dia");
+            String dato = Console.ReadLine();
+            int dia = int.Parse(dato);
+            Console.WriteLine("Mes");
+            dato = Console.ReadLine();
+            int mes = int.Parse(dato);
+            Console.WriteLine("Año");
+            dato = Console.ReadLine();
+            int anyo = int.Parse(dato);
+            if (mes == 1)
+            {
+                mes = 13;
+                anyo -= 1;
+            }else if (mes == 2)
+            {
+                mes = 14;
+                anyo -= 1;
+            }
+            int op1 = ((mes + 1) * 3) / 5;
+            int op2 = anyo / 4;
+            int op3 = anyo / 100;
+            int op4 = anyo / 400;
+            //5.	Sumar el dia, el doble del mes, 
+            //el año, el resultado de la operación 1, 
+            //el resultado de la operación 2, 
+            //menos el resultado de la operación 3 más la operación 4 más 2.
+            int op5 = dia + (mes * 2) + anyo + op1 + op2 - op3 + op4 + 2;
+            int op6 = op5 / 7;
+            int resultado = op5 - (op6 * 7);
+            if (resultado == 0)
+            {
+                Console.WriteLine("SABADO");
+            }else if (resultado == 1)
+            {
+                Console.WriteLine("DOMINGO");
+            }else if ( resultado == 2)
+            {
+                Console.WriteLine("LUNES");
+            }else if (resultado == 3)
+            {
+                Console.WriteLine("MARTES");
+            }else if (resultado == 4)
+            {
+                Console.WriteLine("MIERCOLES");
+            }else if ( resultado == 5)
+            {
+                Console.WriteLine("JUEVES");
+            }else if (resultado == 6)
+            {
+                Console.WriteLine("VIERNES");
+            }
+            else
+            {
+                Console.WriteLine("RESULTADO INCORRECTO");
+            }
         }
 
         static void MayorTresNumeros()
